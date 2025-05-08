485808493ca81a1208d77e367119afc38b1f1730
Mahlaka <samimahlaka@gmail.com>
2025-05-07 20:16:19 -0500

    Add Contact model and create contacts.db in instance folder

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
-from models import Contact
+from flask_sqlalchemy import SQLAlchemy
+from models import Contact, db
 app = Flask(__name__)
 
-def 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db.init_app(app)
 
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
--- models.py
+++ models.py
@@ -3,9 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 db=SQLAlchemy()
 
 class Contact(db.Model):
-    id = db.Column(db.integer, primary_key=True)
-    name = db.Column(db.string(20), nullable=False)
-    phone = db.Column(db.string(20), nullable= False)
-    email = db.Column(db.string(200))
-    address = db.Column(db.string(200))
-    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(20), nullable= False)
+    email = db.Column(db.String(200))
+    address = db.Column(db.String(200))
